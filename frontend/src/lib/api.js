1dc30d758f7edd647e2c6e023b0e9ee657e1cf49
Bosrijal <hrparrijal@gmail.com>
2025-12-16 10:16:40 +0700

    commit

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,15 +1,14 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Tanpa /api di akhir
-  
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 // Request interceptor untuk menambahkan token
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -25,8 +24,8 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
+      localStorage.removeItem("token");
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
